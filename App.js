b9dbfe96bf4db5f1c2948a94e0699284fb836c54
Vinicius <vini383@gmail.com>
2024-04-11 15:02:16 -0300

    feat: app initialized

---
+++ App.js
@@ -0,0 +1,17 @@
+import AppState from './src/appState/AppState';
+
+export default App = () => (
+  
+    <AppState />
+
+  
+);
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#aaa',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
